d395d357a7decf5e5dcac247744275cf61cce2ab
Fatema Sultana Chowdhury <51081858+fsultana44@users.noreply.github.com>
2021-09-15 11:29:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WeatherUtilities = testtry.WeatherUtilities;
+
+namespace testtry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where we are going");
+            WeatherUtilities.Report("Oulu",WeatherUtilities.FahrenheitToCelsius(65), 73);
+            WeatherUtilities.Report("Denver", WeatherUtilities.FahrenheitToCelsius(77), 55);
+            WeatherUtilities.Report("Bologna", 23, 65);
+        }
+
+        
+
+    }
+}
