1b435c8f8c5b50d3cbce506f08063259676cefe4
fsultana44 <fsultana44@gmail.com>
2021-12-01 12:52:14 +0200

    minor changes

---
+++ CS/MyFirstProgram/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
